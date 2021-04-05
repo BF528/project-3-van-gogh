60bcc6c482d337b7b8a9d2fc76c40f50e928e71c
esameth <esameth1@gmail.com>
2021-04-04 23:48:10 -0400

    Added DESeq2

--- Biologist/GSE_filter.R
+++ Biologist/GSE_filter.R
@@ -10,16 +10,38 @@ MA_AHR <- read.csv('/projectnb/bf528/users/van-gogh/project_3/Biologist/limma_le
 MA_DNA <- read.csv('/projectnb/bf528/users/van-gogh/project_3/Biologist/limma_ifosfamide.csv')
 MA_PXR <- read.csv('/projectnb/bf528/users/van-gogh/project_3/Biologist/limma_fluconazole.csv')
 
+# Read in the RNA-seq (DESeq2) files
+RNA_AHR <- read.csv('/projectnb/bf528/users/van-gogh/project_3/Programmer/DESeq_leflunomide.csv')
+RNA_DNA <- read.csv('/projectnb/bf528/users/van-gogh/project_3/Programmer/DESeq_ifosfamide.csv')
+RNA_PXR <- read.csv('/projectnb/bf528/users/van-gogh/project_3/Programmer/DESeq_fluconazole.csv')
+
 files <- list(MA_AHR=MA_AHR, MA_DNA=MA_DNA, MA_PXR=MA_PXR)
 names <- names(files)
 
-write_file <- function(df, name) {
-  # Filter data by adj. p-value
-  df <- subset(df, adj.P.Val < 0.05)
+# Function to filter DEG and write probe sets IDs to a file
+MA_write_file <- function(df, name) {
+  # Filter data by p < 0.05 and abs(log2FC) > 1.5
+  df <- subset(df, P.Value < 0.05 & abs(logFC) > log2(1.5))
   # Get the genes and write it to a file
+  # Do not include row name, col name, or quotations (just a list of ids)
   write.table(df$X, paste0('/projectnb/bf528/users/van-gogh/project_3/Biologist/', name, ".txt"), 
-              row.names=FALSE, col.names=FALSE)
+              row.names=FALSE, col.names=FALSE, quote=FALSE)
 }
 
 # Apply function for each file
-lapply(names(files), function(x) write_file(files[[x]], x))
+lapply(names(files), function(x) MA_write_file(files[[x]], x))
+
+files <- list(RNA_AHR=RNA_AHR, RNA_DNA=RNA_DNA, RNA_PXR=RNA_PXR)
+names <- names(files)
+# Function to filter DEG and write probe sets/gene names to a file
+RNA_write_file <- function(df, name) {
+  # Filter data by p < 0.05 and abs(log2FC) > 1.5
+  df <- subset(df, pvalue < 0.05 & abs(log2FoldChange) > 1.5)
+  # Get the genes and write it to a file
+  # Do not include row name, col name, or quotations (just a list of ids)
+  write.table(df$X, paste0('/projectnb/bf528/users/van-gogh/project_3/Biologist/', name, ".txt"), 
+              row.names=FALSE, col.names=FALSE, quote=FALSE)
+}
+
+# Apply function for each file
+lapply(names(files), function(x) RNA_write_file(files[[x]], x))
