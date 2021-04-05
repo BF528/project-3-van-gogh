cffc039fa66fe01434e35215f5ae56386c9d1584
Monil Gandhi <gandhimonil.2008@gmail.com>
2021-04-05 01:27:16 -0700

    Added comments to the Rfiles

--- analyst_limma.R
+++ analyst_limma.R
@@ -1,3 +1,7 @@
+# Monil Gandhi - Analyst
+
+#File Description - Run limma on microarray data to find the DE genes and write the csv to the analyst folder
+
 # install packages and load libraries
 install.packages("pacman")
 require(pacman)  
@@ -6,7 +10,6 @@ library(reshape2)
 library(grid)
 library(gridExtra)
 install.packages("limma")
-
 library(limma)
 
 #run pacman for downloading the necessary libraries
@@ -14,27 +17,29 @@ pacman::p_load(pacman, dplyr, GGally, ggplot2, ggthemes,
                ggvis, httr, lubridate, plotly, rio, rmarkdown, shiny, 
                stringr, tidyr, ggpubr, grid, gridExtra) 
 
-
+#import the limma package
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
-
 BiocManager::install("limma")
 
-
+#import the group 3 toxo info csv
 samples <- read.csv("/project/bf528/project_3/groups/group_3_mic_info.csv")
 
+#import the rma table
 rma <- read.table('/projectnb/bf528/project_3/samples/liver-normalization-rma.txt',
                   sep='\t',
                   as.is=TRUE,
                   header=TRUE,
                   row.names=1,
 )
+
 # subset the full expression matrix to just those in this comparison
 rma.subset <- rma[paste0('X',samples$array_id)]
 
 #LEFLUNOMIDE
 rma.subset.leflmide <- rma[paste0('X',samples$array_id[samples$chemical=='LEFLUNOMIDE' | (samples$chemical=='Control' & samples$vehicle=='CORN_OIL_100_%')])]
 
+#call design to create a matrix
 design <- model.matrix(
   ~factor(
     samples$chemical[samples$vehicle=='CORN_OIL_100_%'],
@@ -42,6 +47,7 @@ design <- model.matrix(
   )
 )
 
+#assign the column name
 colnames(design) <- c('Intercept','LEFLUNOMIDE')
 
 # run limma
@@ -51,10 +57,13 @@ t <- topTable(fit, coef=2, n=nrow(rma.subset.leflmide), adjust='BH')
 
 # t_adj_pval <- subset(t, adj.P.Val < 0.05)
 
+#get the DE genes on P.Value < 0.05 & abs(logFC) > log2(1.5)
 t_adj_pval <- subset(t, P.Value < 0.05 & abs(logFC) > log2(1.5))
 
+#print the DE genes in LEFLUNOMIDE
 print(nrow(t_adj_pval))
 
+#write the csv to the folder
 write.csv(t,'/projectnb/bf528/users/van-gogh/project_3/Analyst/limma_leflunomide.csv')
 
 
@@ -62,6 +71,7 @@ write.csv(t,'/projectnb/bf528/users/van-gogh/project_3/Analyst/limma_leflunomide
 #FLUCONAZOLE
 rma.subset.fluzole <- rma[paste0('X',samples$array_id[samples$chemical=='FLUCONAZOLE' | (samples$chemical=='Control' & samples$vehicle=='CORN_OIL_100_%')])]
 
+#call design to create a matrix
 design <- model.matrix(
   ~factor(
     samples$chemical[samples$vehicle=='CORN_OIL_100_%'],
@@ -69,6 +79,7 @@ design <- model.matrix(
   )
 )
 
+#assign the column name
 colnames(design) <- c('Intercept','FLUCONAZOLE')
 
 # run limma
@@ -78,16 +89,20 @@ t <- topTable(fit, coef=2, n=nrow(rma.subset.fluzole), adjust='BH')
 
 # t_adj_pval <- subset(t, adj.P.Val < 0.05)
 
+#get the DE genes on P.Value < 0.05 & abs(logFC) > log2(1.5)
 t_adj_pval <- subset(t, P.Value < 0.05 & abs(logFC) > log2(1.5))
 
-
+#print the DE genes in FLUCONAZOLE
 print(nrow(t_adj_pval))
+
+#write the csv to the folder
 write.csv(t,'/projectnb/bf528/users/van-gogh/project_3/Analyst/limma_fluconazole.csv')
 
 
 #IFOSFAMIDE
 rma.subset.isomide <- rma[paste0('X',samples$array_id[samples$chemical=='IFOSFAMIDE' | (samples$chemical=='Control' & samples$vehicle=='SALINE_100_%')])]
 
+#call design to create a matrix
 design <- model.matrix(
   ~factor(
     samples$chemical[samples$vehicle=='SALINE_100_%'],
@@ -95,6 +110,7 @@ design <- model.matrix(
   )
 )
 
+#assign the column name
 colnames(design) <- c('Intercept','IFOSFAMIDE')
 
 # run limma
@@ -104,10 +120,13 @@ t <- topTable(fit, coef=2, n=nrow(rma.subset.isomide), adjust='BH')
 
 # t_adj_pval <- subset(t, adj.P.Val < 0.05)
 
+#get the DE genes on P.Value < 0.05 & abs(logFC) > log2(1.5)
 t_adj_pval <- subset(t, P.Value < 0.05 & abs(logFC) > log2(1.5))
 
-
+#print the DE genes in IFOSFAMIDE
 print(nrow(t_adj_pval))
+
+#write the csv to the folder
 write.csv(t,'/projectnb/bf528/users/van-gogh/project_3/Analyst/limma_ifosfamide.csv')
 
 
--- plots_limma.R
+++ plots_limma.R
@@ -1,3 +1,7 @@
+# Monil Gandhi - Analyst
+
+#File Description - Import the csv from the limma analysis and plot the histograms and volcano plots for the DE genes
+
 # install packages and load libraries
 install.packages("pacman")
 require(pacman)  
@@ -21,49 +25,39 @@ limma_ifosfamide <- read.csv("/projectnb/bf528/users/van-gogh/project_3/Analyst/
 limma_fluconazole <- read.csv("/projectnb/bf528/users/van-gogh/project_3/Analyst/limma_fluconazole.csv")
 
 
+# get the DE gens with P.Value < 0.05 & abs(logFC) > log2(1.5) for each of the three samples
 
 # histogram_leflunomide <- limma_leflunomide[limma_leflunomide$adj.P.Val < 0.05,]
-
 histogram_leflunomide <- limma_leflunomide[limma_leflunomide$P.Val < 0.05 & abs(limma_leflunomide$logFC) > log2(1.5),]
-
-
 # histogram_fluconazole <- limma_fluconazole[limma_fluconazole$adj.P.Val < 0.05,]
-
 histogram_fluconazole <- limma_fluconazole[limma_fluconazole$P.Val < 0.05 & abs(limma_fluconazole$logFC) > log2(1.5),]
-
+# histogram_ifosfamide <- limma_ifosfamide[limma_ifosfamide$adj.P.Val < 0.05,]
 histogram_ifosfamide <- limma_ifosfamide[limma_ifosfamide$P.Val < 0.05 & abs(limma_ifosfamide$logFC) > log2(1.5),]
 
-# histogram_ifosfamide <- limma_ifosfamide[limma_ifosfamide$adj.P.Val < 0.05,]
-
+#Plot the histograms for the DE genes for each of the three samples
 l <- ggplot(histogram_leflunomide, aes(x=logFC))+
   geom_histogram(color="darkblue", fill="lightblue") + labs(title="DE genes - Leflunomide", x="Log Fold Change", y="Count") + theme_classic() +
   theme(plot.title = element_text(hjust = 0.5), axis.title.x=element_text(size=10), axis.title.y=element_text(size=10))
-
 
 f <- ggplot(histogram_fluconazole, aes(x=logFC))+
   geom_histogram(color="darkblue", fill="lightblue") + labs(title="DE genes - Fluconazole", x="Log Fold Change", y="Count") + theme_classic() +
   theme(plot.title = element_text(hjust = 0.5), axis.title.x=element_text(size=10), axis.title.y=element_text(size=10))
 
-
 i <- ggplot(histogram_ifosfamide, aes(x=logFC))+
   geom_histogram(color="darkblue", fill="lightblue") + labs(title="DE genes - Ifosfamide", x="Log Fold Change", y="Count") + theme_classic() +
   theme(plot.title = element_text(hjust = 0.5), axis.title.x=element_text(size=10), axis.title.y=element_text(size=10))
 
-# arrange histograms
+# Plot the histograms on the same plane
 histogram_plane <- ggarrange(l, f, i, 
                       labels = c("1.", "2.", "3."), 
                       ncol = 3, nrow = 1)
 
-histogram_plane
-
 # ggsave("histograms_part5.png", plot = histogram_plane, width=7, height=3, units="in")
 
-
+#Plot the volcano plot for Leflunomide
 row.names(limma_leflunomide) <- limma_leflunomide$X
 
-
 limma_leflunomide_volcano <- subset(limma_leflunomide,select= c("logFC","P.Value"))
-
 
 l <- EnhancedVolcano(limma_leflunomide_volcano,
                 lab = rownames(limma_leflunomide_volcano),
@@ -77,6 +71,7 @@ l <- EnhancedVolcano(limma_leflunomide_volcano,
                 xlim = c(-5,9), widthConnectors = 0.5)
 
 
+#Plot the volcano plot for Fluconazole
 row.names(limma_fluconazole) <- limma_fluconazole$X
 
 limma_fluconazole_volcano <- subset(limma_fluconazole,select= c("logFC","P.Value"))
@@ -92,7 +87,7 @@ f <- EnhancedVolcano(limma_fluconazole_volcano,
                 labSize = 4.0,
                 xlim = c(-4,4), widthConnectors = 0.5)
 
-
+#Plot the volcano plot for Ifosfamide
 row.names(limma_ifosfamide) <- limma_ifosfamide$X
 
 limma_ifosfamide_volcano <- subset(limma_ifosfamide,select= c("logFC","P.Value"))
